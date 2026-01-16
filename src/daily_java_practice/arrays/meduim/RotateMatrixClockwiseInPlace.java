c9ef2252401fe704e71fc4618f0a44c26feaeaa8
Manish Kumar <mk684010@gmail.com>
2026-01-16 23:24:35 +0530

    Day 04 Rotate Matrix by 90 degrees

---
+++ src/daily_java_practice/arrays/meduim/RotateMatrixClockwiseInPlace.java
@@ -0,0 +1,45 @@
+package daily_java_practice.arrays.meduim;
+
+public class RotateMatrixClockwiseInPlace {
+	public static void rotateClockwise(int[][] matrix) {
+
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        rotateClockwise(matrix);
+
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
