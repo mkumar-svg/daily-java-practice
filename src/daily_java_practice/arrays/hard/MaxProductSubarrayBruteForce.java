73448c934e974285b94b46af73c1277c448956e8
Manish Kumar <mk684010@gmail.com>
2026-01-19 13:23:16 +0530

    Day 07 Maximum Product Subarray

---
+++ src/daily_java_practice/arrays/hard/MaxProductSubarrayBruteForce.java
@@ -0,0 +1,22 @@
+package daily_java_practice.arrays.hard;
+
+public class MaxProductSubarrayBruteForce {
+	public static int maxProductSubarray(int[] nums) {
+		int maxProduct = nums[0];
+		
+		for(int i = 0; i < nums.length; i++) {
+			int product = 1;
+			for(int j = i; j < nums.length; j++) {
+				product *= nums[j];
+				maxProduct = Math.max(maxProduct, product);
+			}
+		}
+		return maxProduct;
+	}
+	
+	public static void main(String[] args) {
+        int[] nums = {2, 3, -2, 4};
+
+        System.out.println(maxProductSubarray(nums));
+    }
+}
