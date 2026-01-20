acf3618bfe80ebb08086e31003914f42758f932a
Manish Kumar <mk684010@gmail.com>
2026-01-20 18:36:36 +0530

    Day 08 Check if two strings are anagram of each other

---
+++ src/daily_java_practice/string/easy/SortingBasedAnagramChecker.java
@@ -0,0 +1,33 @@
+package daily_java_practice.string.easy;
+
+import java.util.Arrays;
+
+public class SortingBasedAnagramChecker {
+	public static boolean areAnagrams(String s1, String s2) {
+
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "INTEGER";
+        String s2 = "TEGERNI";
+
+        System.out.println(areAnagrams(s1, s2));
+    }
+}
