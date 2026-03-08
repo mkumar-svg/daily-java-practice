89d5bc90b0512024507e34a54e7a91b4c197aeee
Manish Kumar <mk684010@gmail.com>
2026-03-08 15:48:18 +0530

    Day 13 longest repeating character replacement Better Approach

---
+++ src/daily_java_practice/sliding/window/two/pointer/hard/LongestRepeatingCharacterReplacementSlidingWindow.java
@@ -0,0 +1,34 @@
+package daily_java_practice.sliding.window.two.pointer.hard;
+
+public class LongestRepeatingCharacterReplacementSlidingWindow {
+	public static int characterReplacement(String s, int k) {
+
+        int[] freq = new int[26];
+        int left = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            freq[s.charAt(right) - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
+
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        String s = "AABABBA";
+        int k = 1;
+
+        int result = characterReplacement(s, k);
+        System.out.println(result);
+    }
+}
